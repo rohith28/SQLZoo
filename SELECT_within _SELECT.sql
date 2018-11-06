6199319fb1c4659cb89b10e6cbafa4999b3a62e6
Rohith Uppala <rohithuppala28@gmail.com>
2018-11-06 13:48:14 -0500

    Update SELECT_within _SELECT.sql

--- SELECT_within _SELECT.sql
+++ SELECT_within _SELECT.sql
@@ -22,4 +22,32 @@ WHERE continent IN (SELECT continent
                     WHERE name = 'Argentina' OR name= 'Australia')
 ORDER BY name
 
-4) 
+4) /*Which country has a population that is more than Canada but less than Poland? Show the name and the population.*/
+SELECT name, population
+FROM world
+WHERE population > (SELECT population 
+                    FROM world 
+                    WHERE name = 'Canada') 
+      AND population < (SELECT population 
+                        FROM world 
+                        WHERE name = 'Poland')
+5) /*Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.*/ 
+SELECT name,CONCAT(ROUND((population*100)/(SELECT population 
+                                           FROM world 
+                                           WHERE name ='Germany')),'%')
+FROM world
+WHERE continent = 'Europe'
+
+6) /*Which countries have a GDP greater than every country in Europe? [Give the name only.] (Some countries may have NULL gdp values) */
+SELECT name
+FROM world
+WHERE gdp > (SELECT max(gdp) 
+             FROM world 
+             WHERE continent = 'Europe')
+ 7)/*Find the largest country (by area) in each continent, show the continent, the name and the area*/
+ SELECT continent, name, area 
+ FROM world x
+ WHERE area >= ALL(SELECT area 
+                   FROM world y
+                   WHERE y.continent=x.continent
+                   AND area>0)
